f33af5b078521fc4bc69780ec7f72a5b3c9d27a1
theonetruenerd <33624342+theonetruenerd@users.noreply.github.com>
2023-10-30 10:28:16 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,10 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+pre {                                 
+    white-space: pre-wrap !important; 
+    word-break: break-all;            
+}
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
